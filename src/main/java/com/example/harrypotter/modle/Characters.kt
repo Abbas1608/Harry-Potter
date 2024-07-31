99bbafdae6541e22351f8b6fdb39a464945f1e2d
Abbas Shaikh <158834606+Abbas1608@users.noreply.github.com>
2024-07-31 22:25:15 +0530

    Delete src directory

--- src/main/java/com/example/harrypotter/modle/Characters.kt
+++
@@ -1,8 +0,0 @@
-package com.example.harrypotter.modle
-
-data  class Characters (
-    val displayName:String,
-    val description:String,
-    val developerName:String,
-    val displayIcon:String
-)
