9070de77bcfd8f8179635254890a34bf9bfc971e
Abbas Shaikh <158834606+Abbas1608@users.noreply.github.com>
2024-07-31 16:52:27 +0530

    Add files via upload
    under prosses

---
+++ src/main/java/com/example/harrypotter/screens/CharactersListScreen.kt
@@ -0,0 +1,36 @@
+package com.example.harrypotter.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.harrypotter.modle.Characters
+import com.example.harrypotter.R
+
+
+@Composable
+fun CharactesListScreen(data: Array<Characters>, onClick: (characters: Characters)->Unit) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(100.dp)
+                    .align(CenterHorizontally)
+            )
+            CharactersList(data = data,onClick)
+        }
+    }
+}
